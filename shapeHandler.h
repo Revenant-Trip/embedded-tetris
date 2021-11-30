8834c6d51bbaea01ec4e9b35332670afbbfee88b
Ken Rudnicki <94656597+Revenant-Trip@users.noreply.github.com>
2021-11-30 13:04:12 -0800

    Add files via upload

---
+++ shapeHandler.h
@@ -0,0 +1,18 @@
+#include <ti/devices/msp432p4xx/inc/msp.h>
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+#include <ti/grlib/grlib.h>
+#include "LcdDriver/Crystalfontz128x128_ST7735.h"
+#include <stdio.h>
+
+
+
+/* Graphic library context */
+Graphics_Context g_sContext;
+
+#define width 5
+
+
+
+
+struct Tetromino shapeLib(int shapeSelectVar, int rotation);
+void drawShape(int shapeSelectVar, int positionXY[], int rotation);
